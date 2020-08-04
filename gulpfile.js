08ece3916b4d732bccebd7248584be17af10e029
koniecznytomek <tomek@tomekkonieczny.com>
2020-08-04 20:11:44 +0200

    Add images & fonts build

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const buffer = require('vinyl-buffer');
 const uglify = require('gulp-uglify');
 const htmlmin = require('gulp-htmlmin');
 const sass = require('gulp-sass');
+const rename = require('gulp-rename');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
@@ -17,38 +18,46 @@ const server = browserSync;
 const paths = {
     source: './app',
     build: './build',
+    images: './app/images',
+    fonts: './app/fonts',
 };
 
 const files = {
     scssPath: 'app/scss/**/*.scss',
     jsPath: 'app/js/**/*.js',
     htmlPath: 'app/*.html',
+    imgPath: 'app/images/*.*',
+    fontsPath: 'app/fonts/*.*',
 };
 
-function scssTask() {
+function scssTask(done) {
     return src(files.scssPath)
         .pipe(sourcemaps.init())
-        .pipe(sass())
+        .pipe(sass({ outputStyle: 'expanded' }))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(postcss([autoprefixer(), cssnano()]))
         .pipe(sourcemaps.write('.'))
         .pipe(dest('app/css'));
+    done();
 }
 
 /* Build Tasks */
 
-function htmlBuild() {
+function htmlBuild(done) {
     return src(`${paths.source}/*.html`)
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(dest(paths.build));
+    done();
 }
 
-function cssBuild() {
+function cssBuild(done) {
     return src(`${paths.source}/css/**/*.css`)
         .pipe(postcss([cssnano()]))
         .pipe(dest(`${paths.build}/css`));
+    done();
 }
 
-function javascriptBuild() {
+function javascriptBuild(done) {
     return browserify({
         entries: [`${paths.source}/js/script.js`],
         transform: [babelify.configure({ presets: ['@babel/preset-env'] })],
@@ -58,6 +67,17 @@ function javascriptBuild() {
         .pipe(buffer())
         .pipe(uglify())
         .pipe(dest(`${paths.build}/js`));
+    done();
+}
+
+function imagesBuild(done) {
+    src(files.imgPath).pipe(dest(`${paths.build}/images`));
+    done();
+}
+
+function fontsBuild(done) {
+    src(files.fontsPath).pipe(dest(`${paths.build}/fonts`));
+    done();
 }
 
 function reload(done) {
@@ -76,10 +96,16 @@ function serve(done) {
 
 function watchTask() {
     watch(
-        [files.scssPath, files.htmlPath, files.jsPath],
+        [files.scssPath, files.htmlPath, files.jsPath, files.imgPath],
         series(parallel(scssTask, reload))
     );
 }
 
 exports.default = series(parallel(scssTask, serve), watchTask);
-exports.build = parallel(javascriptBuild, htmlBuild, cssBuild);
+exports.build = parallel(
+    javascriptBuild,
+    htmlBuild,
+    cssBuild,
+    imagesBuild,
+    fontsBuild
+);
